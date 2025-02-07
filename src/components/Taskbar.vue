a3452e72decf1d4192470a5d91038b7c188c17d9
hung vu <hungvugithub@gmail.com>
2025-02-08 00:08:56 +0700

    active animation when click img

--- src/components/Taskbar.vue
+++ src/components/Taskbar.vue
@@ -9,7 +9,7 @@
 			<img src="/windows-start.webp" alt="" />
 		</div>
 		<div class="item active">
-			<img src="/file_explorer.png" alt="" style="width: 85%;" />
+			<img src="/file_explorer.png" alt="" style="width: 32px;" />
 		</div>
 		<div class="item">
 			<img src="/chrome.png" alt="" />
@@ -82,10 +82,11 @@
 
 				&.focus {
 					background-color: rgba(255, 255, 255, 0.1);
-					box-shadow: 0px 0px 1px 0.5px rgba(255,255,255,0.2) inset;
+					box-shadow: 0px 0px 0px 0.25px rgba(255, 255, 255, 0.15) inset;
 
 					&:hover {
-						background-color: rgba(255, 255, 255, 0.2);
+						background-color: rgba(255, 255, 255, 0.15);
+						box-shadow: none;
 					}
 
 					&::before {
@@ -95,11 +96,30 @@
 				}
 
 				img {
-					width: 70%;
+					width: 26px;
 					display: block;
 					padding-bottom: 2px;
+					transition: all ease 0.15s;
+				}
+
+				&:active {
+					img {
+						transform: scale(0.8);
+					}
 				}
 			}
+		}
+	}
+
+	@keyframes click-taskbar-icon {
+		0% {
+			transform: scale(1);
+		}
+		50% {
+			transform: scale(0.5);
+		}
+		100% {
+			transform: scale(1);
 		}
 	}
 </style>
