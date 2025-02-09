84de9a41d7bc07cdc4805eb3fb3e41655e8f5996
hung vu <hungvugithub@gmail.com>
2025-02-09 17:14:07 +0700

    use typescript

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
-<script setup>
-  import Taskbar from './components/Taskbar.vue';
+<script>
+  import Taskbar from '@/components/Taskbar.vue';
+
+  export default {
+      name: "App",
+      components: { Taskbar }
+  };  
 </script>
 
 <template>
   <Taskbar />
 </template>
 
-<style scoped>
+<style scoped lang="ts">
 </style>
---
+++ src/types/TaskbarItem.ts
@@ -0,0 +1,7 @@
+type TaskbarItem = {
+	name: string,
+	image: string,
+	style: string,
+	isActive: boolean | null,
+}
+export default TaskbarItem;
